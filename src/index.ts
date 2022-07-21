1f5c5db136363cac0c89dd37dc73b6b54b2c38c2
Joschua H <joschua.hass.sh@gmail.com>
2022-07-21 09:21:46 +0200

    added GetLocationsByAddress to client

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { ApiConfig } from "./ApiConfig";
+import { GetLocationsByAddress } from "./routes/GetLocationsByAddress";
 
 class FoodZoneApiClient {
     Config = new ApiConfig();
+
+    async getLocationsByAddress(address: string) {
+        var route = new GetLocationsByAddress();
+        return await route.getData(address);
+    }
 }
 
 export const FoodZone = new FoodZoneApiClient();
